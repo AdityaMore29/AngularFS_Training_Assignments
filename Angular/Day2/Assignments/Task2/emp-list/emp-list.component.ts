c095d5d515afc4b66c8663acbbca27ff33beb795
AdityaMore29 <104132363+AdityaMore29@users.noreply.github.com>
2022-04-28 17:56:21 +0530

    Add files via upload

---
+++ Angular/Day2/Assignments/Task2/emp-list/emp-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { employee } from '../models/employee';
+
+@Component({
+  selector: 'app-emp-list',
+  templateUrl: './emp-list.component.html',
+  styleUrls: ['./emp-list.component.css']
+})
+export class EmpListComponent implements OnInit, OnChanges {
+@Input()
+sortoption:string="";
+EmpArrycopy:employee[]=[];
+  EmpArr: employee[] = [
+    { EmpId: 1, EmpName: "James", Salary: 15000, Job: "Manager", DeptNo: 1 },
+    { EmpId: 2, EmpName: "Marie", Salary: 20000, Job: "Manager", DeptNo: 1 },
+    { EmpId: 3, EmpName: "Rose", Salary: 30000, Job: "Developer", DeptNo: 1 },
+    { EmpId: 4, EmpName: "Jeff", Salary: 40000, Job: "Developer", DeptNo: 1 },
+    { EmpId: 5, EmpName: "Paul", Salary: 50000, Job: "QA", DeptNo: 3 },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.sortoption !=""){
+      alert("check in Emp-List");
+      this.EmpArrycopy=this.EmpArr.sort(function(a,b){return a.EmpId < b.EmpId ? 1 :-1});
+      this.EmpArr=this.EmpArrycopy;
+    }
+  }
+
+}
