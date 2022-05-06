45680d59b8fd5f423ac86f88e9c48e98589f6b21
AdityaMore29 <104132363+AdityaMore29@users.noreply.github.com>
2022-05-06 17:33:22 +0530

    Create app.component.ts

---
+++ Angular/Day7/Assignments/Task2/app.component/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My-Project';
+  prodCat:string= "category1";
+  sortopt:string="";
+
+  SendSortdata(sortdata:string){
+    this.sortopt=sortdata;
+    }
+    names:string[]=["Scott", "Smith", "Robert", "Mark","Sam"];
+}
+
+
