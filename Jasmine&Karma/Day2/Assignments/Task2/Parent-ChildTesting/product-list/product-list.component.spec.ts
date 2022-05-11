d271ef34019cb7b38ce8c3d2c0c41236fa3622b1
AdityaMore29 <104132363+AdityaMore29@users.noreply.github.com>
2022-05-11 15:18:59 +0530

    Add files via upload

---
+++ Jasmine&Karma/Day2/Assignments/Task2/Parent-ChildTesting/product-list/product-list.component.spec.ts
@@ -0,0 +1,59 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { ProductListComponent } from './product-list.component';
+
+describe('ProductListComponent', () => {
+  let component: ProductListComponent;
+  let fixture: ComponentFixture<ProductListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+    // 2. Test child component loading
+    it('should render child component elements -- product-details', () => {
+      const childComponent  = fixture.debugElement.query(By.css('app-product-details'));
+      expect(childComponent).toBeTruthy();
+    });
+
+     // 3. Test all child components
+  it('should render all child component elements -- product-details', () => {
+    const childComponents  = fixture.debugElement.queryAll(By.css('app-product-details'));
+    expect(childComponents.length).toBe(component.productArr.length);
+  });
+
+  it('testing child component data -- product-details', () => {
+    const testObj:any = { PId : 1, PName: "product1", Price: 100, Category: "category1", Qty: 4 };
+    const childComponent  = fixture.debugElement.query(By.css('app-product-details'));
+    
+    expect(childComponent.properties["prodObj"]).toEqual( component.productArr[0] );
+  });
+
+  // 5. Test child component events
+  it('testing child component events -- onProdRemove', () => {
+    const childComponent  = fixture.debugElement.query(By.css('app-product-details'));
+    const pId = childComponent.properties["prodObj"].PId;
+
+    //console.log("Before remove : "  + component.depts.length);
+    childComponent.triggerEventHandler('onProdRemoved', pId)
+    // console.log("After remove : "  + component.depts.length);
+
+    let index = component.productArr.findIndex( (item:any) => item.PId == pId);
+    expect(index).toBe(-1);
+  });
+
+});
